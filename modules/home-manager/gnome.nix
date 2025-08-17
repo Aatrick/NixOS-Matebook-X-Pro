e4faac0452abed576a7d129396a9ee68c6352bbf
Aatrick <melis.emilio1@gmail.com>
2025-08-17 19:23:39 +0200

    Remove tiling-shell extension and related configurations from GNOME setup

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -26,7 +26,6 @@
     gnomeExtensions.user-themes
     gnomeExtensions.places-status-indicator
     gnomeExtensions.quick-settings-audio-panel
-    gnomeExtensions.tiling-shell
     # Icons
     pkgs-unstable.papirus-icon-theme
   ];
@@ -45,7 +44,6 @@
 	          light-style.extensionUuid
 	          places-status-indicator.extensionUuid
             quick-settings-audio-panel.extensionUuid
-            tiling-shell.extensionUuid
         ];
       };
       "org/gnome/desktop/interface" = {
@@ -69,74 +67,6 @@
        	dash-max-icon-size = 32;
        	background-opacity = 0.3;
 	      };
-      "org/gnome/shell/extensions/tilingshell" = {
-            enable-autotiling = true;
-            show-indicator = false;
-            enable-screen-edges-windows-suggestions = true;
-            enable-smart-window-border-radius = true;
-            enable-snap-assistant-windows-suggestions = true;
-            enable-tiling-system-windows-suggestions = true;
-            enable-window-border = false;
-            focus-window-down = ["<Control><Super>Down"];
-            focus-window-left = ["<Control><Super>Left"];
-            focus-window-right = ["<Control><Super>Right"];
-            focus-window-up = ["<Control><Super>Up"];
-            highlight-current-window = ["''"];
-            inner-gaps = lib.hm.gvariant.mkUint32 6;
-            snap-assistant-threshold = lib.hm.gvariant.mkInt32 20;
-            layouts-json = builtins.toJSON [
-                {
-                    id =  "2 windows";
-                    tiles = [
-                        {
-                            x = 0;
-                            y = 0;
-                            width = 0.5663145539906104;
-                            height = 1;
-                            groups = [1];
-                        }
-                        {
-                            x = 0.5663145539906104;
-                            y = 0;
-                            width = 0.4336854460093899;
-                            height = 1;
-                            groups = [2 1];
-                        }
-                    ];
-                }
-                {
-                    id =  "3 windows";
-                    tiles = [
-                        {
-                            x = 0;
-                            y = 0;
-                            width = 0.5663145539906104;
-                            height = 1;
-                            groups = [1];
-                        }
-                        {
-                            x = 0.5663145539906104;
-                            y = 0;
-                            width = 0.4336854460093899;
-                            height = 0.4995159728944821;
-                            groups = [2 1];
-                        }
-                        {
-                            x = 0.5663145539906104;
-                            y = 0.4995159728944821;
-                            width = 0.4336854460093899;
-                            height = 0.500484027105518;
-                            groups = [2 1];
-                        }
-                    ];
-                }
-            ];
-            selected-layouts = [["2 windows"] ["2 windows"]];
-            outer-gaps = lib.hm.gvariant.mkUint32 6;
-            overriden-window-menu = true;
-            top-edge-maximise = true;
-            untile-window = ["<Super>d"];
-        };
       "org/gnome/desktop/privacy".hide-identity = true;
       "org/gnome/SessionManager".logout-prompt = false;
       "org/gnome/shell/extensions/blur-my-shell/dash-to-dock".blur = false;
