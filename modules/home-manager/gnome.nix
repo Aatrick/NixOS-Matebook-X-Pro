8906a4054233528fc177f8aa89b3f6ca803cbe2a
Aatrick <melis.emilio1@gmail.com>
2025-08-21 14:59:48 +0200

    Add weather-oclock extension and adjust text scaling factor in GNOME configuration

--- modules/home-manager/gnome.nix
+++ modules/home-manager/gnome.nix
@@ -26,6 +26,7 @@
     gnomeExtensions.user-themes
     gnomeExtensions.places-status-indicator
     gnomeExtensions.quick-settings-audio-panel
+    gnomeExtensions.weather-oclock
     # Icons
     pkgs-unstable.papirus-icon-theme
   ];
@@ -44,6 +45,7 @@
 	          light-style.extensionUuid
 	          places-status-indicator.extensionUuid
             quick-settings-audio-panel.extensionUuid
+            weather-oclock.extensionUuid
         ];
         favorite-apps = ["vesktop.desktop"
             "spotify.desktop"
@@ -70,7 +72,7 @@
             show-battery-percentage = true;
             toolbar-style = "text";
             gtk-theme = "Adwaita";
-            text-scaling-factor = 0.85;
+            text-scaling-factor = 0.95;
         };
       "org/gnome/desktop/app-folders" ={
         folder-children = ["System" "Utilities" "YaST" "Pardus" "SysApps"];
@@ -87,6 +89,7 @@
         "org/desktop/vm/preferences" = {
             button-layout = "appmenu:minimize,maximize,close";
         };
+      "org/gnome/shell/extensions/weather-oclock/weather-after-clock" = true;
       "org/gnome/shell/extensions/dash-to-dock" = {
        	show-icons-emblems = false;
       	  show-show-apps-button = false;
