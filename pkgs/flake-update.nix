5ae7716bb58081aa1982135af26ac1df021d04be
Aatricks <melis.emilio1@gmail.com>
2025-12-13 12:31:03 +0100

    reverterd update command

--- pkgs/flake-update.nix
+++ pkgs/flake-update.nix
@@ -2,24 +2,25 @@
 
 pkgs.writeShellScriptBin "flake-update" ''
     cd ${flake_path}
-    if ! ${pkgs.git}/bin/git diff-index --quiet HEAD --; then
-      ${pkgs.coreutils}/bin/echo "The repository contains uncommitted changes. Cancelled."
-      exit 1
-    fi
+    # if ! ${pkgs.git}/bin/git diff-index --quiet HEAD --; then
+    #   ${pkgs.coreutils}/bin/echo "The repository contains uncommitted changes. Cancelled."
+    #   exit 1
+    # fi
 
     ${pkgs.nix}/bin/nix flake update --flake ${flake_path}
 
     ${pkgs.git}/bin/git add flake.lock > /dev/null 2>&1
     ${pkgs.git}/bin/git commit -m "update" > /dev/null 2>&1
 
-    if sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${flake_path}\#${flake_config} --impure; then
-      ${pkgs.git}/bin/git push > /dev/null 2>&1
-      ${pkgs.coreutils}/bin/echo "Updated successfully"
-      ${nix-latest-update}/bin/nix-latest-update
-      exit 0
-    else
-      ${pkgs.coreutils}/bin/echo "Update failed"
-      ${pkgs.git}/bin/git reset --hard HEAD~1 > /dev/null 2>&1
-      exit 1
-    fi
+    sudo ${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake ${flake_path}\#${flake_config} --impure;
+    # then
+    #   ${pkgs.git}/bin/git push > /dev/null 2>&1
+    #   ${pkgs.coreutils}/bin/echo "Updated successfully"
+    #   ${nix-latest-update}/bin/nix-latest-update
+    #   exit 0
+    # else
+    #   ${pkgs.coreutils}/bin/echo "Update failed"
+    #   ${pkgs.git}/bin/git reset --hard HEAD~1 > /dev/null 2>&1
+    #   exit 1
+    # fi
 ''
