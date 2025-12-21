7306e4cdcc62c205e0742360d6b9028c03780c76
Aatricks <melis.emilio1@gmail.com>
2025-12-21 15:52:08 +0100

    Add common Home Manager profile

---
+++ modules/home-manager/profiles/common.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+
+{
+  imports = [
+    ../gnome.nix
+    ../git.nix
+    ../dev.nix
+    ../shell.nix
+    ../flake-script.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.stateVersion = "25.11";
+}
