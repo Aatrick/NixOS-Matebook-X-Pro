1b3cfc2e75eb0c38b085086b13af04bb77f0f8c4
Aatricks <melis.emilio1@gmail.com>
2025-12-26 14:08:20 +0100

    Enable Android module and vendor fish plugins
    Comment out android-studio while keeping android-tools. Move fishPlugins
    out of home.packages and add vendored sources: fisher
    (jorgebucaran/fisher rev 4.4.4) and plugin sources tide, done, fzf-fish,
    and forgit.

--- modules/home-manager/android.nix
+++ modules/home-manager/android.nix
@@ -1,6 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    android-studio
+    # android-studio
     android-tools
   ];
 }
--- modules/home-manager/shell.nix
+++ modules/home-manager/shell.nix
@@ -7,10 +7,6 @@
     grc
     fish
     fzf
-    fishPlugins.done
-    fishPlugins.fzf-fish
-    fishPlugins.forgit
-    fishPlugins.tide
   ];
   programs.fish = {
     enable = true;
@@ -26,6 +22,31 @@
           rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
           sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
         };
+      }
+      {
+        name = "fisher";
+        src = pkgs.fetchFromGitHub {
+          owner = "jorgebucaran";
+          repo = "fisher";
+          rev = "4.4.4";
+          hash = "sha256-e8gIaVbuUzTwKtuMPNXBT5STeddYqQegduWBtURLT3M=";
+        };
+      }
+      {
+        name = "tide";
+        src = pkgs.fishPlugins.tide.src;
+      }
+      {
+        name = "done";
+        src = pkgs.fishPlugins.done.src;
+      }
+      {
+        name = "fzf-fish";
+        src = pkgs.fishPlugins.fzf-fish.src;
+      }
+      {
+        name = "forgit";
+        src = pkgs.fishPlugins.forgit.src;
       }
     ];
     shellAliases = {
